4b4c28b66c7afc9e67f1455bdc17a11f8e8617cd
Santiago-SanP05 <santiagopinzoncruz@gmail.com>
2025-02-11 09:19:06 -0500

    fix: :bug: fix a bug
    @Santiago-SanP05

--- src/main/java/com/campus/restaurante/dto/OrderProductDTO.java
+++ src/main/java/com/campus/restaurante/dto/OrderProductDTO.java
@@ -7,4 +7,37 @@ public class OrderProductDTO {
     private String ProductName;
     private int quantity;
 
+    public OrderProductDTO() {
+    }
+
+    public OrderProductDTO(Long idOrdersProducts, String ProductName, int quantity) {
+        this.idOrdersProducts = idOrdersProducts;
+        this.ProductName = ProductName;
+        this.quantity = quantity;
+    }
+
+    public Long getIdOrdersProducts() {
+        return idOrdersProducts;
+    }
+
+    public void setIdOrdersProducts(Long idOrdersProducts) {
+        this.idOrdersProducts = idOrdersProducts;
+    }
+
+    public String getProductName() {
+        return ProductName;
+    }
+
+    public void setProductName(String ProductName) {
+        this.ProductName = ProductName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
 }
