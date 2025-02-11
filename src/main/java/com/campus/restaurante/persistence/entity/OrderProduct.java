2f494b234e9d526baf667dc6ac122c455d0f6e5f
Santiago-SanP05 <santiagopinzoncruz@gmail.com>
2025-02-11 08:17:48 -0500

    feat: :bug: Add contenet entity Order Products
    Add contenet entity Order Products
    
    @Santiago-SanP05

--- src/main/java/com/campus/restaurante/persistence/entity/OrderProduct.java
+++ src/main/java/com/campus/restaurante/persistence/entity/OrderProduct.java
@@ -4,10 +4,64 @@
  */
 package com.campus.restaurante.persistence.entity;
 
-/**
- *
- * @author camper
- */
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
 public class OrderProduct {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idOrdersProducts;
+    
+    @ManyToOne
+    @JoinColumn(name = "idProduct")
+    private Products product;
+    
+    @ManyToOne
+    @JoinColumn(name = "idOrder")
+    private Orders order;
+    
+    private int quantity;
+
+    public OrderProduct(Long idOrdersProducts, Products product, Orders order, int quantity) {
+        this.idOrdersProducts = idOrdersProducts;
+        this.product = product;
+        this.order = order;
+        this.quantity = quantity;
+    }
+
+    public Long getIdOrdersProducts() {
+        return idOrdersProducts;
+    }
+
+    public void setIdOrdersProducts(Long idOrdersProducts) {
+        this.idOrdersProducts = idOrdersProducts;
+    }
+
+    public Products getProduct() {
+        return product;
+    }
+
+    public void setProduct(Products product) {
+        this.product = product;
+    }
+
+    public Orders getOrder() {
+        return order;
+    }
+
+    public void setOrder(Orders order) {
+        this.order = order;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
 }
