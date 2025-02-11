8bd85baf8d9fb7df9ac28230e8e87f8f3b6d97ae
Santiago-SanP05 <santiagopinzoncruz@gmail.com>
2025-02-11 08:04:36 -0500

    feat: :sparkles: Add Entit
    Add Entity Product
    
    Add Entity Product

---
+++ src/main/java/com/campus/restaurante/persistence/entity/Products.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.campus.restaurante.persistence.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Products {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProduct;
+    
+    private String name;
+    private Double price;
+    private String details;
+
+    public Products(Long idProduct, String name, Double price, String details) {
+        this.idProduct = idProduct;
+        this.name = name;
+        this.price = price;
+        this.details = details;
+    }
+
+    public Long getIdProduct() {
+        return idProduct;
+    }
+
+    public void setIdProduct(Long idProduct) {
+        this.idProduct = idProduct;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+    
+    
+}
